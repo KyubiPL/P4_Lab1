2df29d078e1ad49d2b66211af9cc355f5b12f518
Daniel <daniel.lipowczan@gmail.com>
2020-02-08 17:34:33 +0100

    commit

---
+++ P4_Lab1/Crud.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace P4_Lab1
+{
+    class Crud
+    {
+        public void Create(int id,string regionDescription,SqlConnection conn)
+        {
+            string sql = "INSERT INTO Region(RegionID,RegionDescription) VALUES(@id,@regionName)";
+            var command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@regionName", regionDescription);
+            command.Parameters.AddWithValue("@id", id);
+
+            int x=command.ExecuteNonQuery();
+
+            if (x > 0)
+            {
+                Console.WriteLine("Wpisano");
+            }
+        }
+
+        public void Read(SqlConnection conn)
+        {
+            SqlCommand command = new SqlCommand
+            {
+                CommandText = "SELECT * FROM Customers",
+                Connection = conn
+            };
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Stanowisko"]);
+            }
+            reader.Close();
+        }
+        public void Update(int id, string newName,SqlConnection conn)
+        {
+            string sql = "UPDATE Region SET RegionDescription=@regionName WHERE RegionID=@id ";
+            var command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("id", id);
+            command.Parameters.AddWithValue("regionName", newName);
+
+            int x = command.ExecuteNonQuery();
+
+            if (x > 0)
+            {
+                Console.WriteLine("Zmieniono");
+            }
+        }
+
+        public void Delete(int id,SqlConnection conn)
+        {
+            string sql = "DELETE FROM Region Wjere RegionID=@id";
+            var command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("id", id);
+            int x = command.ExecuteNonQuery();
+            if (x > 0)
+            {
+                Console.WriteLine("Usunieto");
+            }
+
+        }
+    }
+}
---
+++ P4_Lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace P4_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                @"Data Source=DESKTOP-647LNP9\MSSQLSERVER01;Initial Catalog=BD1z_so_1A;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+
+            SqlCommand command = new SqlCommand
+            {
+                CommandText = "SELECT * FROM Pracownicy",
+                Connection = conn
+            };
+
+           
+            Crud crud = new Crud();
+            crud.Create(5,"Slask", conn);
+            crud.Update(5, "Slask", conn);
+            crud.Delete(5, conn);
+
+            conn.Close();
+        }
+    }
+}
